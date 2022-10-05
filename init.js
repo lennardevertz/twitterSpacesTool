0abaafffd23f50797520c27d2bf7fc80c2730151
lennardevertz <levertz@idriss.xyz>
2022-10-05 14:43:58 +0200

    fix port

--- init.js
+++ init.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || '3000'
 
 const app = express()
 
-app.set("port", PORT)
-
 app.use(function (req, res, next) {
     //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
     res.setHeader('Access-Control-Allow-Origin', '*')
@@ -20,13 +18,19 @@ app.get('*', async (req, res) => {
     try {
         let params = req.path.split('/');
         let roomId = params[params.length - 1]
-        if (!watchingRooms.has(roomId)) {
+        if (!watchingRooms.has(roomId) && roomId != "favicon.ico") {
+            console.log("Listening to room ", roomId)
             watchingRooms.add(roomId);
             watchRoom(roomId).catch(ex=>null);
         }
         res.send(await getRoomData(roomId));
     } catch (e) {
+    console.log(e)
         res.sendStatus(500)
     }
 })
 
+
+const server = app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
